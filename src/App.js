0bd3f78e270b4b1b03ddd6be3af9245c3c876aba
abid <fmaabid@gmail.com>
2018-09-25 17:33:01 +0600

    Working eith Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Person from './Person/Person';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi React</h1>
+        <Person name="Abid" age="23" />
+        <Person name="Atik" age="24" />
+        <Person name="Shahed" age="25" />
       </div>
     );
   }
