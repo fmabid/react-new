fbc4ad82b402674aad031c9a48679b026fd4df10
abid <fmaabid@gmail.com>
2018-09-27 00:04:50 +0600

    Outputting lists

--- src/App.js
+++ src/App.js
@@ -52,17 +52,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Abid!!!')}
-            changed={this.nameChangedHandler}>My Hobbies: Programming</Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age} />
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              age={person.age} />
+          })}
         </div>
       );
     }
